03a933190759c8c50402714cc6016964f20d92d4
xph8 <xpheight@gmail.com>
2023-12-10 01:52:39 +0530

    pipx managed packages

---
+++ .chezmoiscripts/common/run_onchange_after_pipx.sh
@@ -0,0 +1,27 @@
+#! /bin/sh
+
+# update with python: {{ output "python" "--version" }}
+declare -a packages=(
+    "black"
+    "isort"
+
+    "numpy"
+    "scipy"
+    "pandas"
+    "matplotlib"
+    "seaborn"
+
+    "notebook"
+    "sphinx"
+
+    "requests"
+    "pytest"
+)
+
+for package in "${packages[@]}"; do
+    if pipx list | grep "package $package"; then
+        pipx reinstall $package --python $(which python)
+    else
+        pipx install $package --include-deps --python $(which python)
+    fi
+done
